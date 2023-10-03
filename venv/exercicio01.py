aa59c3bc695476f682ec08d46b872620212b2181
alexandregpeixoto <alexandregpeixoto@gmail.com>
2023-10-02 21:23:25 -0300

    Exercicio 01

---
+++ venv/exercicio01.py
@@ -0,0 +1,5 @@
+# Escreva um programa que peça ao usuário sua idade e informe se ele é maior de idade ou não.
+
+nome = input("Digite o nome:")
+idade = input("Digite a idade:")
+print("O nome é " + nome + " e sua idade é de " + str(idade) + " anos.")
